f6faa848309d834b6785f7a451b9d33b62b78601
chenping <>
2025-08-18 15:56:57 +0800

    ✨ feat(occ真值生成):  增加数据流功能
    新增：1. 点云动静态分离；2. 点云mesh重建；3. 点云自车点云剔除；4.地面分割；5. 点云及mesh可视化

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 ## Occ3D
 https://tsinghua-mars-lab.github.io/Occ3D/
+
+
 Occ3D: A Large-Scale 3D Occupancy Prediction Benchmark for Autonomous Driving
+
+
 重点关注其数据集
+
+
 提出了一个更为通用的Occ数据生成方式，可以用于生成不同场景下的高密度、所见即所得的Occ数据。主要包括三个步骤：1）体素稠密化，2）遮挡推理，3）图片引导refine
+
 ### Method
 分别基于nuscenes和waymo数据集生成Occ数据集，具体参数如图:
 ![alt text](pics/image-1.png)除了原有数据集中分割的类别，还增加了通用类别(general object，GO)的标注。
 其中，nuscenes数据集的标注范围为[-40m, -40m, -1m, 40m, 40m, 5.4m]； waymo的标注范围为[-80m, -80m, -1m, 80m, 80m, 5.4m]
+
 **Dataset Construction Pipeline**
+
 完整的数据生成流程如图所示：
 ![alt text](pics/image-2.png)
 由于图像缺乏可靠的深度和尺度信息，因此仅利用图像进行Occ真值生成是不可能的。因此本文利用点云和其分割结果重建高质量的Occ真值，并解决了几个关键问题：1） 点云稀疏性问题，2） 遮挡问题，3） 点云与图像之间的对齐问题。
@@ -19,12 +28,16 @@ Occ3D: A Large-Scale 3D Occupancy Prediction Benchmark for Autonomous Driving
     重建网格后，进行密集点采样，并再次利用KNN为采样点分配标签。
 
 - Occlusion Reasoning for Visibility Mask
+
 本文提出了遮挡推理，并分别应用于雷达和相机视角，从而生成可见性掩码。
+
 ![alt text](pics/image-3.png)
+
 由于点云的稀疏性，很容易将空洞误以为是"free"，因此本文采用叠帧数据下的射线广播的方法，如图a所示，如果该点位置反射了来自雷达的射线，则定义为"occupied"，若an该点被射线o穿透，则定义为"free"，除此之外，则定义为"unobserved"。
 我们将每个被占据的体素中心与摄像机原点连接起来，从而形成一条射线。沿着每条射线，我们将第一个被占据的体素设为 "observed"，其余的设为"unobserved"。相机射线未扫描到的任何体素也会被设置为 "unobserved"。
 
 -  Image-guided Voxel Refinement
+
 由于雷达噪声和位姿偏移的影响，点云数据对于物体的观测会比实际物理尺寸要大，因此本文引入了图像引导的体素细化方法对结果进行细化和去噪。![alt text](pics/image-4.png)
 同样采用射线广播的方法，但这次我们使用的是相机射线，并使用相机图像作为参考。
 ### Experiment
--- load_nuscenes.py
+++ load_nuscenes.py
@@ -1,10 +1,16 @@
 from collections import defaultdict
 from os import path as osp
+import pickle
+from tkinter import _flatten
 import pyquaternion
 from scipy.spatial.transform import Rotation as R
+from tqdm import tqdm
+from tqdm.contrib.concurrent import thread_map
+from functools import partial
 
 
-
+import vdbfusion
+import open3d as o3d
 import numpy as np
 from torch.utils.data import Dataset
 from nuscenes import NuScenes
@@ -17,9 +23,23 @@ from mmcv.utils import Registry, build_from_cfg
 from mmdet3d.core.bbox import get_box_type
 from mmdet3d.core.bbox import LiDARInstance3DBoxes
 from mmdet3d.datasets.pipelines import Compose
+from mmdet3d.core.bbox import box_np_ops
+import pypatchworkpp
 
-from mmdetection3d.tools.misc.browse_dataset import show_det_data, show_result
+from ops.mmdetection3d.tools.misc.browse_dataset import show_det_data, show_result
 # @DATASETS.register_module()
+
+
+def rotate_yaw(yaw):
+    if yaw > np.pi:
+        yaw -= 2 * np.pi
+    elif yaw < -np.pi:
+        yaw += 2 * np.pi
+    return np.array(
+        [[np.cos(yaw), np.sin(yaw), 0], [-np.sin(yaw), np.cos(yaw), 0], [0, 0, 1]],
+        dtype=np.float32,
+    )
+
 
 class LiDARInstance3DBoxeswithID(LiDARInstance3DBoxes):
     def __init__(self, tensor, box_dim=7, with_yaw=True, origin=(0.5, 0.5, 0), ids=None):
@@ -103,11 +123,13 @@ class SequenceDataset(Dataset):
             # print(info['token'])
             sweeps = info.pop("sweeps")
             info['annos'] = {'gt_boxes': info.pop('gt_boxes'), 'gt_names':info.pop('gt_names'), "gt_inst_token":info.pop('gt_inst_token')}
+            info["is_key_frame"] = True
             out.append(info)
             for sweep in sweeps:
                 sweep['lidar_path'] = sweep['data_path']    
                 sweep['lidar2ego_translation'] = sweep['sensor2ego_translation']
                 sweep['lidar2ego_rotation'] = sweep['sensor2ego_rotation']
+                sweep["is_key_frame"] = False
                 out.append(sweep)
         return out
     
@@ -118,21 +140,6 @@ class SequenceDataset(Dataset):
         return len(self.data_infos)
     
     def pre_pipeline(self, results):
-        """Initialization before data preparation.
-
-        Args:
-            results (dict): Dict before data preprocessing.
-
-                - img_fields (list): Image fields.
-                - bbox3d_fields (list): 3D bounding boxes fields.
-                - pts_mask_fields (list): Mask fields of points.
-                - pts_seg_fields (list): Mask fields of point segments.
-                - bbox_fields (list): Fields of bounding boxes.
-                - mask_fields (list): Fields of masks.
-                - seg_fields (list): Segment fields.
-                - box_type_3d (str): 3D box type.
-                - box_mode_3d (str): 3D box mode.
-        """
         results['img_fields'] = []
         results['bbox3d_fields'] = []
         results['pts_mask_fields'] = []
@@ -144,20 +151,6 @@ class SequenceDataset(Dataset):
         results['box_mode_3d'] = 0
 
     def get_data_info(self, index):
-        """Get data info according to the given index.
-
-        Args:
-            index (int): Index of the sample data to get.
-
-        Returns:
-            dict: Data information that will be passed to the data
-                preprocessing pipelines. It includes the following keys:
-
-                - sample_idx (str): Sample index.
-                - pts_filename (str): Filename of point clouds.
-                - file_name (str): Filename of point clouds.
-                - ann_info (dict): Annotation info.
-        """
         info = self.data_infos[index]
         
         lidar2global = self.get_lidar2global(index)
@@ -176,22 +169,10 @@ class SequenceDataset(Dataset):
         input_dict['ann_info'] = annos
             # if self.filter_empty_gt and ~(annos['gt_labels_3d'] != -1).any():
                 # return None
+        # print(f"token:{info['token']}, is key frame:{info['is_key_frame']}")
         return input_dict
     
     def get_ann_info(self, index):
-        """Get annotation info according to the given index.
-
-        Args:
-            index (int): Index of the annotation data to get.
-
-        Returns:
-            dict: Annotation information consists of the following keys:
-
-                - gt_bboxes_3d (:obj:`LiDARInstance3DBoxes`):
-                    3D ground truth bboxes
-                - gt_labels_3d (np.ndarray): Labels of ground truths.
-                - gt_names (list[str]): Class names of ground truths.
-        """
         info = self.data_infos[index]
         gt_bboxes_3d = info["annos"]['gt_boxes']
         gt_names_3d = info["annos"]['gt_names']
@@ -224,6 +205,10 @@ def init_dataset(data, token):
                 coord_type='LIDAR',
                 load_dim=5,
                 use_dim=5),
+            dict(type="RemoveSelfCenter", radius=2.5),
+            dict(
+                type='RemoveGround',
+            ),
             # dict(
             #     type='LoadPointsFromMultiSweeps',
             #     sweeps_num=10,
@@ -236,33 +221,226 @@ def init_dataset(data, token):
                 with_label_3d=True),
             dict(type='GlobalAlignmentwithGT',
                  ),
+            
+
         ])
     
     dataset = build_dataset(dataset_cfg)
     return dataset
 
+
+def get_mesh(points, voxel_size=0.1, sdf_trunc=0.3, min_weight=0):
+    # min_weight = 0
+    # voxel_size = 0.08
+    # sdf_trunc = 0.1
+    vdb_volume = vdbfusion.VDBVolume(voxel_size=voxel_size,
+                                    sdf_trunc=sdf_trunc,
+                                    space_carving=False)
+    for i, point in enumerate(tqdm(points)):
+        if len(point) > 0:
+            vdb_volume.integrate(point[:, :3].astype(np.float64), np.eye(4))
+    vertices, triangles = vdb_volume.extract_triangle_mesh(fill_holes=True, min_weight=min_weight)
+    mesh = o3d.geometry.TriangleMesh(
+        o3d.utility.Vector3dVector(vertices),
+        o3d.utility.Vector3iVector(triangles),
+        )
+    mesh.compute_vertex_normals()
+    return mesh
+
+def get_ground_mesh(ground_points, grid_size=5):
+    ground_points = ground_points[np.logical_and(ground_points[:, 2] > -1.5, ground_points[:, 2] < 1.5)]
+    region_range = np.stack([ground_points[:, :3].min(axis=0), ground_points[:, :3].max(axis=0)])
+    x_edges = np.arange(region_range[0, 0], region_range[1, 0], grid_size)
+    y_edges = np.arange(region_range[0, 1], region_range[1, 1], grid_size)
+    region_grid = np.meshgrid(x_edges, y_edges, indexing='ij')
+    grid_indices = np.stack((
+        np.digitize(ground_points[:, 0], bins=x_edges) - 1,
+        np.digitize(ground_points[:, 1], bins=y_edges) - 1,), axis=-1)
+    ground_points2grid_map = defaultdict(list)
+    for i, idx in enumerate(grid_indices):
+        pos = (x_edges[idx[0]], y_edges[idx[1]])
+        ground_points2grid_map[pos].append(i)
+    # ret = []
+    # for x_, y_ in :
+
+    def thread_worker(i, region_grid=None, ground_points2grid_map=None, points=None):
+        x_, y_ = region_grid[i]
+        idx = np.array(ground_points2grid_map[(x_, y_)])
+        if len(idx) < 2:
+            return None
+        points_ = ground_points[np.array(ground_points2grid_map[(x_, y_)])]
+        print(points_.shape)
+        ground_pcd = o3d.geometry.PointCloud()
+        ground_pcd.points = o3d.utility.Vector3dVector(points_[:, :3])    # show_result(points=all_points, gt_bboxes=None, pred_bboxes=None, out_dir="./viz", filename=example['sample_idx'], show=True, snapshot=True)
+        ground_pcd.estimate_normals()
+    # with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
+        ground_mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(ground_pcd, depth=9)
+        ground_mesh.compute_vertex_normals()
+        return ground_mesh
+    region_grid = np.stack((region_grid), -1).reshape(-1, 2)
+    thread_func = partial(thread_worker, region_grid=region_grid, ground_points2grid_map=ground_points2grid_map, points=ground_points)
+    ret = []
+    for i in range(len(region_grid)):
+        mesh_ = thread_func(i)
+        if mesh_ is not None:
+            ret.append(mesh_)
+    # ret = thread_map(thread_func, range(len(region_grid)), max_workers=1, chunksize=20)
+    # 
+        # ret.append(ground_mesh)
+    return ret
+# def get_voxel(mesh_points, voxel_size=0.1):
+    # voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(mesh_points,
+    #                                                         voxel_size=0.05)
+
+
 def main(data_path, info_path):
+
     dataset = SeqNuscene(data_path, info_path, nuscenes_version="v1.0-mini")
     for i in track_iter_progress(list(range(len(dataset)))):
         seq_dataset = dataset[i]
+        obj_points_bank = {}
+        ground_points_bank = {}
+        nonground_points_bank = {}
         for j in track_iter_progress(list(range(len(seq_dataset)))):
             input_dict = seq_dataset.get_data_info(j)
             seq_dataset.pre_pipeline(input_dict)
-            example = seq_dataset.pipeline(input_dict)        
+            example = seq_dataset.pipeline(input_dict)
+            
             annos = example['ann_info']
             image_idx = example['sample_idx']
             points = example['points'].tensor.numpy()
             gt_boxes_3d = annos['gt_bboxes_3d'].tensor
             names = annos['gt_names']
-            show_result(points=points, gt_bboxes=gt_boxes_3d.clone(), pred_bboxes=None, out_dir="./viz", filename=example['sample_idx'], show=True, snapshot=True)
+            gt_boxes_id = annos['gt_bboxes_id']
+            obj_point_indices = box_np_ops.points_in_rbbox(points, gt_boxes_3d.numpy())
+
+            #! 区分地面/非地面, 前景(标注目标)/背景点云
+            background_points_indices = np.where(~obj_point_indices.any(axis=1))[0]
+            ground_points_indices = example['ground_idx']
+            nonground_points_indices = example['nonground_idx']
+            background_and_ground_points_indices = np.intersect1d(ground_points_indices, background_points_indices, assume_unique=True)
+            background_and_nonground_points_indices = np.intersect1d(nonground_points_indices, background_points_indices, assume_unique=True)
+            # left_points = example['points'][ground_points_indices]
+            # left_points = points[background_points_indices]
+            nonground_points_bank[example['timestamp']] =  points[background_and_nonground_points_indices]
+            ground_points_bank[example['timestamp']] = points[background_and_ground_points_indices]
+            # show_result(points=points, gt_bboxes=gt_boxes_3d.clone(), pred_bboxes=None, out_dir="./viz", filename=example['sample_idx'], show=True, snapshot=True)
+            
+            for i, (obj_id, box) in enumerate(zip(gt_boxes_id, gt_boxes_3d.numpy())):
+                if obj_id not in obj_points_bank:
+                    obj_points_bank[obj_id] = {'points':[], "size": box[3:6]}
+                gt_points = points[obj_point_indices[:, i]]
+                gt_points[:, :3] -= box[:3]
+                gt_points[:, :3] = gt_points[:, :3] @ rotate_yaw(box[6]).T
+                obj_points_bank[obj_id]['points'].append(gt_points)
+                obj_points_bank[obj_id]['size'] = np.maximum(obj_points_bank[obj_id]['size'], box[3:6])
+                pass
+        
+        #! 序列背景点云拼接验证
+        # all_points = list(background_points_bank.values())
+        # all_points = np.vstack(all_points)
+
+        #! 地面点二次筛选
+        all_points = list(nonground_points_bank.values())
+        PatchworkPLUSPLUS = pypatchworkpp.patchworkpp(pypatchworkpp.Parameters())
+        batch_id = np.concatenate([np.full(len(points), i) for i, points in enumerate(all_points)])
+        all_points_arr = np.vstack(all_points)
+        PatchworkPLUSPLUS.estimateGround(all_points_arr[:, :4])
+        nonground_idx = PatchworkPLUSPLUS.getNongroundIndices()
+        nonground_points = all_points_arr[nonground_idx]
+        ground_idx = PatchworkPLUSPLUS.getGroundIndices()
+        batch_id = batch_id[nonground_idx]
+        all_points = [nonground_points[batch_id == i] for i in range(len(nonground_points_bank))]
+        #! 背景非地面点云mesh重建
+        nonground_mesh = get_mesh(all_points)
+        
+        
+        # viz_mesh(nonground_mesh)
+        ground_points = list(ground_points_bank.values())
+        ground_points = np.vstack(ground_points)
+        ground_points = np.concatenate((ground_points, all_points_arr[ground_idx]))
+        ground_mesh = get_ground_mesh(ground_points)
+        viz_mesh(ground_mesh)
+        for obj_id, data in obj_points_bank.items():
+            points = data['points']
+            size = data['size']
+            mesh = get_mesh(points)
+            # points = np.vstack(points)
+            show_result(points=np.vstack(points), gt_bboxes=None, pred_bboxes=None, out_dir="./viz", filename=example['sample_idx'], show=True, snapshot=True)
+            o3d.visualization.draw_geometries([mesh])  
+            o3d.io.write_triangle_mesh(f"./viz/{obj_id}.ply", mesh)
 
 
+def viz_mesh(mesh, gt_box=None, save=True):
+
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+        size=1, origin=[0, 0, 0])  # create coordinate frame
+    vis.add_geometry(mesh_frame)
+    if isinstance(mesh, list):
+        for m in mesh:
+            vis.add_geometry(m)
+    else:
+        vis.add_geometry(mesh)
+    if gt_box is not None:
+        gt_box[0, 2] += 0.5 * gt_box[0, 5]
+        box3d = o3d.geometry.OrientedBoundingBox(gt_box[0, 0:3], np.eye(3), gt_box[0, 3:6])
+        line_set = o3d.geometry.LineSet.create_from_oriented_bounding_box(box3d)
+        vis.add_geometry(line_set)
+    vis.run()
+    # o3d.visualization.draw_geometries([mesh, mesh, mesh])  
+    if save:
+        o3d.io.write_triangle_mesh(f"./viz/tmp.ply", mesh)
+    return 
 
 if __name__ == '__main__':
-    from mmdetection3d.tools.data_converter.nuscenes_converter import create_nuscenes_infos
+    from ops.mmdetection3d.tools.data_converter.nuscenes_converter import create_nuscenes_infos
     create_nuscenes_infos(
-        '/media/chen/Elements/nuScenes/raw/Trainval/',
-        info_prefix="test",)
-        # version='v1.0-mini',)
+        # '/media/chen/Elements/nuScenes/raw/Trainval/',
+        "/media/chen/data/nusecnes/v1.0-mini",
+        info_prefix="test",
+        version='v1.0-mini',
+        get_seg=True)
 
-    # main('/media/chen/data/nusecnes/v1.0-mini/', 'test_infos_train.pkl')
+    main('/media/chen/data/nusecnes/v1.0-mini/', 'test_infos_train.pkl')
+    obj_points_bank = pickle.load(open("tmp.pkl", "rb"))
+    for obj_id, data in obj_points_bank.items():
+    # if True:
+        # obj_id = "e91afa15647c4c4994f19aeb302c7179"
+        # data = obj_points_bank[obj_id]
+        points = data['points']
+        gt_box = np.zeros((1, 7))
+        gt_box[0, 3:6] = data['size']
+        # size = data['size']
+        mesh = get_mesh(points)
+        # mesh 重建
+        viz_mesh(mesh, gt_box.copy(), save=True)
+        # 原始点云体素化
+        viz_points = np.vstack(points)
+        mesh_points = o3d.geometry.PointCloud()
+        mesh_points.points = o3d.utility.Vector3dVector(viz_points[:,:3])
+        voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(mesh_points,
+                                                            voxel_size=0.05)
+        viz_mesh(voxel_grid, gt_box.copy(), save=False)
+        
+        # 原始点云
+        show_result(points=viz_points, gt_bboxes=gt_box.copy(), pred_bboxes=None, out_dir="./viz", filename=obj_id, show=True, snapshot=True)
+        
+        mesh_points = mesh.sample_points_poisson_disk(10000)
+        points = np.asarray(mesh_points.points)
+        points = points[np.logical_and.reduce([points[:, 0] >= -gt_box[0, 3] / 2, points[:, 0] <= gt_box[0, 3] / 2, points[:, 1] >= -gt_box[0, 4] / 2, points[:, 1] <= gt_box[0, 4] / 2, points[:, 2] >= 0, points[:, 2] <= gt_box[0, 5] ])]
+        mesh_points.points = o3d.utility.Vector3dVector(np.vstack((points, viz_points[:,:3])))
+        voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(mesh_points,
+                                                            voxel_size=0.05)
+        
+        # mesh点云+原始点云
+        viz_points = np.asarray(mesh_points.points)
+        show_result(points=viz_points, gt_bboxes=gt_box.copy(), pred_bboxes=None, out_dir="./viz", filename=obj_id, show=True, snapshot=True)
+        # (mesh点云+原始点云i)mesh重建
+        viz_mesh(voxel_grid, gt_box.copy(), save=False)
+
+        # mesh = get_mesh([viz_points, ])
+        # viz_mesh(mesh, gt_box.copy(), save=True)
+        
+        
